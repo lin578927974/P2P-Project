66dbcac7bafee1bffac6b1bbd1723bd8a8f57aff
lin578927974 <578927974@qq.com>
2016-07-11 00:04:41 +0800

    首页添加

--- WebContent/page/index.jsp
+++ WebContent/page/index.jsp
@@ -4,9 +4,28 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>系统管理</title>
+<link rel="stylesheet" type="text/css" href="../css/site.css"/>
+<link rel="stylesheet" type="text/css" href="../css/themes/blue/easyui.css"/>
+<script src="../js/jquery-1.8.2.min.js" type="text/javascript"></script>
+<script src="../js/easyui/jquery.easyui.min-1.3.2.js" type="text/javascript"></script>
 </head>
-<body>
-登录
+<body style="width:100%;height:100%">
+	<!-- 头部 -->
+	<div style="height:60px;">
+	</div>
+	
+	<!-- 中部布局 -->
+	<div style="width:100%;height:100%">
+		<div class="easyui-layout" style="width:100%;height:100%">
+			<div data-options="region:'west',title:'菜单栏',split:true" style="width:200px;">
+			</div>
+			<div data-options="region:'center',title:'首页',">
+			</div>
+		</div>
+	</div>
+	<!-- 底部 -->
+	<div style="height:30px;">
+	</div>
 </body>
 </html>
---
+++ src/com/p2p/beans/sysmodule.java
@@ -0,0 +1,103 @@
+package com.p2p.beans;
+
+public class sysmodule {
+	private  String  MdlCode;
+	private String MdlName;
+	private String Description;
+	private String AnotherCode;
+	private String AbbName;
+	private String EnName;
+	private Byte MdlType;
+	private Byte MdlClass;
+	private String UpCode;
+	private String LinkUrl;
+	private String IcoUrl;
+	private Short OrderDesc;
+	private Byte State;
+	private String Remark;
+	
+	public String getMdlCode() {
+		return MdlCode;
+	}
+	public void setMdlCode(String mdlCode) {
+		MdlCode = mdlCode;
+	}
+	public String getMdlName() {
+		return MdlName;
+	}
+	public void setMdlName(String mdlName) {
+		MdlName = mdlName;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public String getAnotherCode() {
+		return AnotherCode;
+	}
+	public void setAnotherCode(String anotherCode) {
+		AnotherCode = anotherCode;
+	}
+	public String getAbbName() {
+		return AbbName;
+	}
+	public void setAbbName(String abbName) {
+		AbbName = abbName;
+	}
+	public String getEnName() {
+		return EnName;
+	}
+	public void setEnName(String enName) {
+		EnName = enName;
+	}
+	public Byte getMdlType() {
+		return MdlType;
+	}
+	public void setMdlType(Byte mdlType) {
+		MdlType = mdlType;
+	}
+	public Byte getMdlClass() {
+		return MdlClass;
+	}
+	public void setMdlClass(Byte mdlClass) {
+		MdlClass = mdlClass;
+	}
+	public String getUpCode() {
+		return UpCode;
+	}
+	public void setUpCode(String upCode) {
+		UpCode = upCode;
+	}
+	public String getLinkUrl() {
+		return LinkUrl;
+	}
+	public void setLinkUrl(String linkUrl) {
+		LinkUrl = linkUrl;
+	}
+	public String getIcoUrl() {
+		return IcoUrl;
+	}
+	public void setIcoUrl(String icoUrl) {
+		IcoUrl = icoUrl;
+	}
+	public Short getOrderDesc() {
+		return OrderDesc;
+	}
+	public void setOrderDesc(Short orderDesc) {
+		OrderDesc = orderDesc;
+	}
+	public Byte getState() {
+		return State;
+	}
+	public void setState(Byte state) {
+		State = state;
+	}
+	public String getRemark() {
+		return Remark;
+	}
+	public void setRemark(String remark) {
+		Remark = remark;
+	}
+}
