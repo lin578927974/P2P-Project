f98be922e409a50a65afb2806f5cc2765cc63501
lin578927974 <578927974@qq.com>
2016-07-12 00:11:03 +0800

    首页文件添加

---
+++ src/com/p2p/action/SysAccountAction.java
@@ -0,0 +1,14 @@
+package com.p2p.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SysAccountAction extends ActionSupport {
+
+	/**
+	 * 首页请求，加载菜单
+	 *  
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+}
