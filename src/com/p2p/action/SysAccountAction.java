2dbfdb921cd30081500b29aebc19c04b6b25595b
lin578927974 <578927974@qq.com>
2016-07-21 00:13:05 +0800

    文件添加

--- src/com/p2p/action/SysAccountAction.java
+++ src/com/p2p/action/SysAccountAction.java
@@ -1,8 +1,18 @@
 package com.p2p.action;
 
-import com.opensymphony.xwork2.ActionSupport;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class SysAccountAction extends ActionSupport {
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.p2p.beans.sysmodule;
+import com.p2p.service.SysmoduleService;
+
+public class SysAccountAction extends ActionSupport implements ServletRequestAware{
 
 	/**
 	 * 首页请求，加载菜单
@@ -10,6 +20,32 @@ public class SysAccountAction extends ActionSupport {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private SysmoduleService sysmoduleService;
+	public void setSysmoduleService(SysmoduleService sysmoduleService){
+		this.sysmoduleService=sysmoduleService;
+	}
+	
+	private HttpServletRequest request;
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+	private Map<String, Object> map;
+	public Map<String, Object> getMap(){
+		return map;
+	}
+	
+	public String GetTree(){
+		try{
+			String upcode=request.getParameter("id");
+			List<sysmodule> list=sysmoduleService.getSysModule(upcode);
+			map=new HashMap<String, Object>();
+			map.put("list", list);
+			return SUCCESS;
+		}catch(Exception ex){
+			return ERROR;
+		}
+	}
 	
 
 }
--- src/com/p2p/serviceImpl/SysmoduleServiceImpl.java
+++ src/com/p2p/serviceImpl/SysmoduleServiceImpl.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.p2p.beans.sysmodule;
+import com.p2p.dao.SysModuleDao;
 import com.p2p.service.SysmoduleService;
 
 public class SysmoduleServiceImpl implements SysmoduleService {
+	private SysModuleDao sysModuleDao;
+	
+	public void setsysModuleDao(SysModuleDao sysModuleDao){
+		this.sysModuleDao=sysModuleDao;
+	}
+	
 	public List<sysmodule> getSysModule(String id){
-		List<sysmodule> list=new ArrayList<sysmodule>();
+		List<sysmodule> list=sysModuleDao.getModule(id);
 		return list;
 	}
 }
